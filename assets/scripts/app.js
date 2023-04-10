22b9d9cdf6ba903b2069f6a24fcf8449a6d9fbe3
Moise-code <moisewowe@gmail.com>
2023-04-10 13:55:09 +0200

    refactoring the codes to using the class and objects as its instances

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,36 +1,48 @@
-// lets first create the array of the products this will be pasted in the object that we will create later on
 
-// const products = [
-//   {
-//     title: 'Ferari portofino',
-//     ImageUrl: 'https://hips.hearstapps.com/hmg-prod/images/2021-ferrari-portofino-m-117-1621623712.jpg?crop=0.720xw:0.540xh;0.104xw,0.364xh&resize=1200:*',
-//     price:23.5,
-//     description:'This is a nive ferari incase you miss it.'
-//   },
-//   {
-//     title: 'Mercedes-Benz C-Class',
-//     ImageUrl: 'https://www.topgear.com/sites/default/files/2021/11/Mercedes_C300D_0000.jpg',
-//     price:40,
-//     description:'This is a smooth sport car you have ever taste.'
-//   },
-// ]
+
+// lets create the class which will be the blueprint of the object when I talk about blueprint I mean defining the way properties and methods of the object
+// are going to look like.
+
+class Product {
+  title = 'Default'; // this is the default property which is the instance
+  imageUrl;
+  description;
+  price;
+
+  // since the properties will change we are going to create a method to use which is a constructor method
+  // the constructor method is there to help with taking arguments that will be changed since the title or the image Url is not going to be the same everywhere.
+  // that is why in the constructor curry bracels we are going to assign the properties of the class to the arguments of cpnstructor values.
+
+  constructor(title,image,desc,price){
+      this.title = title;
+      this.imageUrl = image;
+      this.description = desc;
+      this.price = price
+
+  }
+}
+// how to add that class then as the new object then, you just go where you need it and use new then class name remember this will be assigned as a new object with same properties as the reference class.
+
+
 
 // now that we created the array for the products, now we are going to create the object to store that the above commented array
 
 const productsList = {
   products: [
-    {
-      title: 'Ferari portofino',
-      ImageUrl: 'https://hips.hearstapps.com/hmg-prod/images/2021-ferrari-portofino-m-117-1621623712.jpg?crop=0.720xw:0.540xh;0.104xw,0.364xh&resize=1200:*',
-      price: 23.5,
-      description: 'This is a nive ferari incase you miss it.'
-    },
-    {
-      title: 'Mercedes-Benz C-Class',
-      ImageUrl: 'https://www.topgear.com/sites/default/files/2021/11/Mercedes_C300D_0000.jpg',
-      price: 40,
-      description: 'This is a smooth sport car you have ever taste.'
-    },
+    // then since the products are the ones which hold object of on product then 
+    // we are going to call the instance of a class here using the new keyword
+    new Product(
+                'Ferari portofino',
+                'https://hips.hearstapps.com/hmg-prod/images/2021-ferrari-portofino-m-117-1621623712.jpg?crop=0.720xw:0.540xh;0.104xw,0.364xh&resize=1200:*',
+                'This is a nive ferari incase you miss it.',
+                23.5
+                ),
+    new Product(
+                'Mercedes-Benz C-Class',
+                'https://www.topgear.com/sites/default/files/2021/11/Mercedes_C300D_0000.jpg',
+                'This is a smooth sport car you have ever taste.',
+                50
+                )  
   ],
 
   // now we are going to create a render method as the third element of the object productsList
@@ -53,7 +65,7 @@ const productsList = {
       // lets then use innerHtml to add the obect literals
       productElement.innerHTML = `
   <div>
-    <img src="${prod.ImageUrl}" alt="${prod.title}">
+    <img src="${prod.imageUrl}" alt="${prod.title}">
     
     <div class="product-item__content">
     
